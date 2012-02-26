890c0d1b841ea04ee47737a278a7e1529955e062
unknown <irina@irina-VAIO.(none)>
2012-02-26 13:50:47 +0200

    init

--- hezenhotel.py
+++ hezenhotel.py
@@ -27,5 +27,6 @@ def rooms():
 	
 	
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
